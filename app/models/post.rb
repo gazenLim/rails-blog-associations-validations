56986c96f889555d42e67fea49b2901be87f48f6
gajen <meetgajen@yahoo.com>
2021-04-30 22:52:46 -0400

    commit tree 9ee84343f5f1188b0d1e497df13f605b646d9c9d

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,18 @@
 class Post < ActiveRecord::Base
-  
+  has_many :post_tags
+  has_many :tags, through: :post_tags
+  belongs_to :user
+
+  validates :name, presence: true
+    validates :content, presence: true
+
+    def tags_attributes=(tags_attributes)
+      tags_attributes.values.each do |tags_attribute|
+        tag = Tag.find_or_create_by(tags_attribute)
+        self.tags << tag
+      end
+    end
+
+
+
 end
